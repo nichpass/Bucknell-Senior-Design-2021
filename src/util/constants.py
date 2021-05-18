d676fb64c214a4e9a013c52c77c9028284ae67ee
Nicholas Passantino <npp002@bucknell.edu>
2021-05-18 00:50:23 -0400

    added updated threshold values

--- src/util/constants.py
+++ src/util/constants.py
@@ -3,12 +3,19 @@ BBOX_AVG_SIZE = {
  'Effusion': 72002.74056476016,
  'Mass': 44378.91808337311,
  'Nodule': 5468.77115036753,
- 'Atelectasis': 33508.51688697086
+ 'Atelectasis': 33508.51688697086,
+ 'Infiltration': 101381.53439058688,
+ 'Pneumonia': 98815.9175028455,
+ 'Pneumothorax': 54644.57334728914
 }
 
-BEST_THRESHOLD = {'Cardiomegaly': 0.23546557128429413,
- 'Effusion': 0.23394592106342316,
- 'Mass': 0.5300890207290649,
- 'Nodule': 0.16681720316410065,
- 'Atelectasis': 0.14686164259910583}
+BEST_THRESHOLD = {'Cardiomegaly': 0.020983250811696053,
+                  'Effusion': 0.054538335651159286,
+                  'Mass': 0.0566093847155571,
+                  'Nodule': 0.08178110420703888,
+                  'Atelectasis': 0.023930028080940247,
+                  'Infiltration': 0.13815227150917053,
+                  'Pneumonia': 0.005487028509378433,
+                  'Pneumothorax': 0.10205517709255219,
+                  'No Finding': 0.4392490088939667}
  #'No Finding': 0.09936013072729111}
